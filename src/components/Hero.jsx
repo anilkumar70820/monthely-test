970da0dfaabc8ab1c7d37367e563ba67cca2d9a7
anilkumar70820 <ak9565191@gmail.com>
2023-12-04 21:39:21 +0530

    add img size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
 <h1 className='ff_playfair_display lh_normal fw-semibold text-white fs_xl text-center'>"Best New NFT to look out for"</h1>
 <p className='fs_xsm fw-semibold text-white lh_120 ff_josefin_sans pb-1 text-center hero_para_max_w'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
 <div className='d-flex  justify-content-center my-4 py-2'>
-    <img src={logo} alt="logo" /> 
+    <img className='hero_img_size' src={logo} alt="logo" /> 
 </div>
 <div className='d-flex justify-content-center'>
     <button className='join_us_btn ff_josefin_sans fs_sm fe-semibold opacity-50 text-white lh_normal'>JOIN US</button>
